913a4fda619e013a9c2655adb4f73c1c9c8f1bb3
skyisland74 <jyejun74@gmail.com>
2025-03-28 10:55:13 +0900

    Update 게임용 봇.py

--- 게임용 봇.py
+++ 게임용 봇.py
@@ -9,17 +9,17 @@ TOKEN = 'access_token'
 
 
 
-# 역할의 ID
-FIRST_GRADE_ROLE_ID = 1234942571172855890  # 1등급 역할의 ID로 실제 ID를 넣어주세요
-SECOND_GRADE_ROLE_ID = 1234942599811432539  # 2등급 역할의 ID로 실제 ID를 넣어주세요
-THIRD_GRADE_ROLE_ID = 1234942622016081961  # 3등급 역할의 ID로 실제 ID를 넣어주세요
+
+FIRST_GRADE_ROLE_ID = 1234942571172855890  
+SECOND_GRADE_ROLE_ID = 1234942599811432539  
+THIRD_GRADE_ROLE_ID = 1234942622016081961  
 FOURTH_GRADE_ROLE_ID = 1234942647039299656
 FIFTH_GRADE_ROLE_ID = 1234942650906443886
 SIXTH_GRADE_ROLE_ID = 1234942700973854750
 SEVENTH_GRADE_ROLE_ID = 1234942727259422720
 EIGHTH_GRADE_ROLE_ID = 1234942748935847946
 
-# 당첨 확률 (20% = 0.2, 10% = 0.1, 5% = 0.05, 2% = 0.02, 1% = 0.01)
+
 WIN_PROBABILITIES = {
     FIRST_GRADE_ROLE_ID: 0.0008,
     SECOND_GRADE_ROLE_ID: 0.0028,
@@ -34,12 +34,12 @@ WIN_PROBABILITIES = {
 
 
 
-# 봇 생성
+
 bot = commands.Bot(command_prefix='/', intents=nextcord.Intents.default())
 
 @bot.slash_command(description="ㄱ쩌는 권한을 얻어보세요!")
 async def 뽑기(ctx: nextcord.Interaction):
-    # 역할마다 당첨 여부 결정
+   
     for role_id, probability in WIN_PROBABILITIES.items():
         if random.random() < probability:
             role = ctx.guild.get_role(role_id)
@@ -50,16 +50,16 @@ async def 뽑기(ctx: nextcord.Interaction):
                 await ctx.send(f'{role.name} 역할을 찾을 수 없습니다. 관리자에게 문의하세요.')
             break
     else:
-        # 어떤 역할도 당첨되지 않았을 경우
+        
         await ctx.send(f'{ctx.user.mention}, 아쉽지만 당첨되지 않았습니다.')
 
 
-# 봇이 준비되었을 때 실행
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name}이(가) 준비되었습니다.')
 
-# 봇 실행
+
 bot.run(TOKEN)
 
 
